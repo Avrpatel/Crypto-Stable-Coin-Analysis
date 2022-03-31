de786bd32a823e6612d6d6c0f88cbb2cf8a072f0
Avrpatel <avinash2772@gmail.com>
2022-03-30 21:04:06 -0400

    git ignore file and data_collection file

---
+++ data_collection.py
@@ -0,0 +1,12 @@
+import panel as pn
+pn.extension('plotly')
+import plotly.express as px
+import pandas as pd
+import hvplot.pandas
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+
